c66587491baad4b7d841e06fe628ade536de82df
79percent <per7614@163.com>
2023-06-18 00:49:51 +0800

    feat: playIcon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import styles from './index.less';
 import VConsole from 'vconsole';
 import * as React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import flvjs from 'flv.js';
+import { useLocation } from 'umi';
+
+let timer: string | number | NodeJS.Timeout | undefined;
 
 export default function IndexPage() {
+  const location = useLocation() as any;
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [showIcon, setShowIcon] = useState(true);
+  const flvPlayer = useRef<flvjs.Player>();
+
   useEffect(() => {
     const vConsole = new VConsole();
     return () => {
@@ -13,28 +21,57 @@ export default function IndexPage() {
   }, []);
 
   useEffect(() => {
-    if (flvjs.isSupported()) {
-      var videoElement = document.getElementById(
-        'videoElement',
-      ) as HTMLMediaElement;
-      var flvPlayer = flvjs.createPlayer({
-        type: 'flv',
-        url: 'http://42.192.22.24:8080/live/livestream/livestream.flv',
-      });
-      flvPlayer.attachMediaElement(videoElement);
-      flvPlayer.load();
-      flvPlayer.play();
-    }
-  }, []);
+    try {
+      if (flvjs.isSupported() && location?.query?.url) {
+        var videoElement = document.getElementById(
+          'videoElement',
+        ) as HTMLMediaElement;
+        flvPlayer.current = flvjs.createPlayer({
+          type: 'flv',
+          url: location?.query?.url,
+          isLive: true,
+        });
+        flvPlayer.current.attachMediaElement(videoElement);
+        flvPlayer.current.load();
+      }
+    } catch (error) {}
+  }, [location?.query?.url]);
 
   return (
     <div className={styles.videoBox}>
       <video
         id="videoElement"
-        autoPlay
+        autoPlay={true}
         controls
         className={styles.video}
+        onTouchEnd={() => {
+          setShowIcon(true);
+          timer = setTimeout(() => {
+            setShowIcon(false);
+          }, 1000);
+        }}
       ></video>
+      {showIcon &&
+        (isPlaying ? (
+          <div
+            className={styles.pause}
+            onClick={() => {
+              setIsPlaying(false);
+              flvPlayer.current?.pause();
+              clearTimeout(timer);
+            }}
+          ></div>
+        ) : (
+          <div
+            className={styles.play}
+            onClick={() => {
+              setIsPlaying(true);
+              flvPlayer.current?.play();
+              setShowIcon(false);
+              clearTimeout(timer);
+            }}
+          ></div>
+        ))}
     </div>
   );
 }
